e49fb03d66e344dbd3e985dc75319fb35283a09e
Manu Cornet <m@ma.nu>
2021-05-29 16:42:26 +0000

    Add some 'cards' to play with

--- IO.js
+++ IO.js
@@ -1,6 +1,18 @@
-const MODULE_ONE_TOTAL_CARD_COUNT = 100;
+const MODULE_ONE_TOTAL_CARD_COUNT = 10;
+const MODULE_ONE_BG_COLORS = [
+  '#ffbcbc', '#ffd6a7', '#fff5a7', '#b8ffa7', '#a7e8ff', '#daa7ff'
+];
 
 function createModuleOneCards() {
+  const moduleOneContainer = document.getElementById('module-one');
+  const storeEl = moduleOneContainer.getElementsByClassName('store')[0];
+  for (let i = 0; i < MODULE_ONE_TOTAL_CARD_COUNT; i++) {
+    let el = document.createElement('div');
+    el.style.backgroundColor = MODULE_ONE_BG_COLORS[
+      i % MODULE_ONE_BG_COLORS.length];
+    el.className = 'module-one-card';
+    storeEl.appendChild(el);
+  }
 }
 
 
